64a740eb92eff80e3596e406f2b02aa47d4699d6
JSA-Code <jsafortech+github@gmail.com>
2023-12-08 19:45:30 -0800

    install sharp disable temporarily /blog page

---
+++ src/app/blog/TEMPpage.jsx
@@ -0,0 +1,13 @@
+// import styles from "./blog.module.css";
+// import CardList from "@/components/cardList/CardList";
+
+// const Blog = () => {
+//   return (
+//     <div className={styles.container}>
+//       <h1 className={styles.title}>Blog</h1>
+//       <CardList />
+//     </div>
+//   );
+// };
+
+// export default Blog;
